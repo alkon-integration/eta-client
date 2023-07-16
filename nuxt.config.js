7629961a6be39ef0408f2def12c98462811f38bd
jcardus <asklocation.net@gmail.com>
2023-07-16 20:56:01 +0100

    SENTRY_AUTH_TOKEN

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extend (config, { isDev }) {
-      if (!isDev) {
+      if (!isDev && process.env.SENTRY_AUTH_TOKEN) {
         config.devtool = 'source-map'
         config.plugins.push(new SentryPlugin({
           org: 'pinme-97',
