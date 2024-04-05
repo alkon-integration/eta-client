6e2b1b2912a46645383d476bfafbcdf72a7d6f86
alkon-integration <139714353+alkon-integration@users.noreply.github.com>
2024-04-05 22:44:14 +0000

    76

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -2,21 +2,18 @@ import Vue from "vue";
 import * as Sentry from "@sentry/vue";
 
 Sentry.init({
-    Vue,
-    dsn: "https://7a563b674cbc0d1f946c40911bc49e0d@o4507029627797504.ingest.us.sentry.io/4507029632122880",
-    integrations: [
+  Vue,
+  dsn: "https://7a563b674cbc0d1f946c40911bc49e0d@o4507029627797504.ingest.us.sentry.io/4507029632122880",
+  integrations: [
       Sentry.browserTracingIntegration(),
       Sentry.replayIntegration({
-        maskAllText: false,
-        blockAllMedia: false,
+      maskAllText: false,
+      blockAllMedia: false,
       }),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: ["localhost", /^https:\/\/polpaico.fleetrack.eu\/api/],
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ['localhost', /^https:\/\/polpaico.fleetrack.eu\/api/],
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
