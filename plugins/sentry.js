685275ca1749cf7a94ea6b729cf4974bfa540c18
alkon-integration <139714353+alkon-integration@users.noreply.github.com>
2024-04-05 23:50:40 +0000

    it works

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -12,9 +12,9 @@ Sentry.init({
         networkDetailAllowUrls: ['/api/devices'],
       })
   ],
-  tracesSampleRate: 1.0, 
+  tracesSampleRate: 0.01, 
   tracePropagationTargets: [/.*/],
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: 0.01, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 0.01, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 })
 
