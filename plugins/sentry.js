458b3eb91e44de7727adef090b21da0ce07b4cbb
alkon-integration <139714353+alkon-integration@users.noreply.github.com>
2024-04-05 22:59:52 +0000

    mapbox.com

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -12,7 +12,7 @@ Sentry.init({
       }),
   ],
   tracesSampleRate: 1.0, 
-  tracePropagationTargets: ['localhost', /^https:\/\/polpaico.fleetrack.eu\/api/],
+  tracePropagationTargets: ['localhost', /^https:\/\/polpaico.fleetrack.eu\/api/, /mapbox.com/],
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 })
